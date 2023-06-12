3c7bde54eee1cd66ee393a46fd1eeaa640feef4f
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2023-06-12 12:16:03 +0300

    set result

--- script.js
+++ script.js
@@ -40,13 +40,16 @@ form.onsubmit = (event) => {
 	});
 
 	// Validate the date format
-	// if (!isValidDateFormat(form.day.value, form.month.value, form.year.value)) {
-	// 	setError(form.day, "Must be a valid date");
-	// 	hasError = true; // Set the flag to true if there is an error
-	// }
+	if (!isValidDateFormat(form.day.value, form.month.value, form.year.value)) {
+		setError(form.day, "Must be a valid date");
+		setError(form.month, "");
+		setError(form.year, "");
+
+		hasError = true; // Set the flag to true if there is an error
+	}
 
 	if (!hasError) {
-		// setRes(form.year.value, form.month.value, form.day.value);
+		setRes(form.year.value, form.month.value, form.day.value);
 	}
 };
 
@@ -56,8 +59,10 @@ function setError(inputElement, errorMessage) {
 
 	inputElement.style.borderColor = "var(--error-color)"; // Apply red border color to input
 	headingElement.style.color = "var(--error-color)"; // Apply red color to the heading element
-	errorElement.textContent = errorMessage; // Update the error message
-	errorElement.style.opacity = "1"; // Display the error message
+	if (errorMessage !== "") {
+		errorElement.textContent = errorMessage; // Update the error message
+		errorElement.style.opacity = "1"; // Display the error message
+	}
 }
 
 function getCurrentYear() {
@@ -65,19 +70,77 @@ function getCurrentYear() {
 }
 
 function isValidDateFormat(day, month, year) {
-	const dateStr = `${year}-${month}-${day}`;
-	const date = new Date(dateStr);
-	return (
-		date.getFullYear().toString() === year &&
-		(date.getMonth() + 1).toString().padStart(2, "0") === month &&
-		date.getDate().toString().padStart(2, "0") === day
-	);
+	let isValid = true;
+	day = parseInt(day);
+	month = parseInt(month);
+	year = parseInt(year);
+
+	if (
+		(month === 2 && isLeapYear(year) && day > 29) ||
+		(month === 2 && day > 28) ||
+		((month === 4 || month === 6 || month === 9 || month === 11) && day > 30)
+	) {
+		isValid = false;
+	}
+	return isValid;
 }
 
-// const setRes = (yearsIn, monthsIn, daysIn) => {
-// 	const date = new Date();
-// 	yearsRes.textContent = date.getFullYear() - yearsIn;
-// 	monthsRes.textContent = Math.abs(date.getMonth() - monthsIn + 1);
-// 	daysRes.textContent = date.getDate() - daysIn;
-// 	console.log(date.getMonth());
-// };
+function isLeapYear(year) {
+	if (year % 4 === 0) {
+		if (year % 100 === 0) {
+			if (year % 400 === 0) {
+				return true; // Divisible by 400, leap year
+			} else {
+				return false; // Divisible by 100 but not by 400, not a leap year
+			}
+		} else {
+			return true; // Divisible by 4 but not by 100, leap year
+		}
+	} else {
+		return false; // Not divisible by 4, not a leap year
+	}
+}
+
+const setRes = (yearInput, monthInput, dayInput) => {
+	const currentDate = new Date();
+	const currentYear = currentDate.getFullYear();
+	const currentMonth = currentDate.getMonth() + 1; // Adding 1 because getMonth() returns zero-based month
+	const currentDay = currentDate.getDate();
+
+	const birthYear = parseInt(yearInput);
+	const birthMonth = parseInt(monthInput);
+	const birthDay = parseInt(dayInput);
+
+	let years = currentYear - birthYear;
+	let months = currentMonth - birthMonth;
+	let days = currentDay - birthDay;
+
+	// Adjust the age if the current month and day are less than the birth month and day
+	if (
+		currentMonth < birthMonth ||
+		(currentMonth === birthMonth && currentDay < birthDay)
+	) {
+		years--;
+		if (currentMonth < birthMonth) {
+			months = 12 - birthMonth + currentMonth;
+		} else {
+			months = 11; // 11 months remaining in the current year
+		}
+	}
+
+	// Adjust the age if the birth day is greater than the current day
+	if (currentDay < birthDay) {
+		months--;
+		const daysInLastMonth = daysInMonth(currentMonth - 1, currentYear);
+		days = daysInLastMonth - birthDay + currentDay;
+	}
+
+	yearsRes.textContent = years;
+	monthsRes.textContent = months;
+	daysRes.textContent = days;
+};
+
+// Function to get the number of days in a specific month and year
+function daysInMonth(month, year) {
+	return new Date(year, month, 0).getDate();
+}
